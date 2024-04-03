e80ab497b6e0814239492e71e8ee7b9ea937ec0a
Guillem <you@example.com>
2024-04-03 12:41:42 -0300

    Remove duplicated port

--- acceptance_test.go
+++ acceptance_test.go
@@ -15,16 +15,19 @@
 package activemq
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"testing"
 	"time"
 
 	sdk "github.com/conduitio/conduit-connector-sdk"
-	"github.com/go-stomp/stomp/v3"
 	"github.com/google/uuid"
 )
+
+
+func uniqueDestinationName(t *testing.T) string {
+	hash := uuid.New().String()[0:8]
+	return fmt.Sprintf("/queue/%s_%s", t.Name(), hash)
+}
 
 func TestAcceptance(t *testing.T) {
 	sourceConfig := map[string]string{
@@ -68,55 +71,57 @@ func TestAcceptance(t *testing.T) {
 	sdk.AcceptanceTest(t, driver)
 }
 
-func uniqueDestinationName(t *testing.T) string {
-	return fmt.Sprintf("/queue/%s_%s", t.Name(), uuid.New().String()[0:8])
-}
+func TestTLSAcceptance(t *testing.T) {
+	sourceConfig := map[string]string{
+		"url":              "localhost:61617",
+		"user":             "admin",
+		"password":         "admin",
+		"subscriptionType": "ANYCAST",
 
-func TestServerTLSConfiguration(t *testing.T) {
-	destination := "/queue/test"
-	conn, err := connect(context.Background(), Config{
-		URL:         "localhost:61617",
-		User:        "admin",
-		Password:    "admin",
-		Destination: destination,
-		TLS: TLSConfig{
-			Enabled:            true,
-			ClientKeyPath:      "./test/certs/client_key.pem",
-			ClientCertPath:     "./test/certs/client_cert.pem",
-			CaCertPath:         "./test/certs/broker_cert",
-			InsecureSkipVerify: true,
+		// we want to disable artemis flow control, so that messages are delivered as soon as possible.
+		// This prevents source reads from timing out in unexpected ways.
+		"consumerWindowSize": "-1",
+
+		"tls.enabled":            "true",
+		"tls.clientKeyPath":      "./test/certs/client_key.pem",
+		"tls.clientCertPath":     "./test/certs/client_cert.pem",
+		"tls.caCertPath":         "./test/certs/broker.pem",
+		"tls.insecureSkipVerify": "true",
+	}
+
+	destinationConfig := map[string]string{
+		"url":             "localhost:61617",
+		"user":            "admin",
+		"password":        "admin",
+		"destinationType": "ANYCAST",
+
+		"tls.enabled":            "true",
+		"tls.clientKeyPath":      "./test/certs/client_key.pem",
+		"tls.clientCertPath":     "./test/certs/client_cert.pem",
+		"tls.caCertPath":         "./test/certs/broker.pem",
+		"tls.insecureSkipVerify": "true",
+	}
+
+	driver := sdk.ConfigurableAcceptanceTestDriver{
+		Config: sdk.ConfigurableAcceptanceTestDriverConfig{
+			Connector:         Connector,
+			SourceConfig:      sourceConfig,
+			DestinationConfig: destinationConfig,
+			BeforeTest: func(t *testing.T) {
+				destination := uniqueDestinationName(t)
+				sourceConfig["destination"] = destination
+				destinationConfig["destination"] = destination
+				destinationConfig["destinationHeader"] = destination
+			},
+			Skip: []string{
+				// Configure tests are faulty since we rely on paramgen to validate required parameters.
+				"TestSource_Configure_RequiredParams",
+				"TestDestination_Configure_RequiredParams",
+			},
+			WriteTimeout: 500 * time.Millisecond,
+			ReadTimeout:  500 * time.Millisecond,
 		},
-	})
-	if err != nil {
-		log.Fatal("Failed to dial to ActiveMQ: ", err)
-	}
-	defer conn.Disconnect()
-
-	// Send a message to a destination
-	message := "Hello, ActiveMQ Artemis with TLS!"
-	err = conn.Send(destination, "text/plain", []byte(message), nil)
-	if err != nil {
-		log.Fatal("Failed to send message:", err)
-	}
-	fmt.Println("Message sent successfully!")
-
-	// Subscribe to the destination
-	sub, err := conn.Subscribe(destination, stomp.AckClient)
-	if err != nil {
-		log.Fatal("Failed to subscribe to destination:", err)
-	}
-	defer sub.Unsubscribe()
-
-	// Receive messages from the subscription
-	for {
-		msg := <-sub.C
-		if msg == nil {
-			break
-		}
-		fmt.Println("Received message:", string(msg.Body))
-		msg.Conn.Ack(msg)
 	}
 
-	// Wait for a short duration before disconnecting
-	time.Sleep(1 * time.Second)
+	sdk.AcceptanceTest(t, driver)
 }
